db0f0627887df41056ef450568926a7abb3681a4
Aditya Agarwal <aditya.agarwal.20012712@gmail.com>
2021-05-14 18:19:19 +0530

    form for mobile number added and connected

--- src/index.js
+++ src/index.js
@@ -8,6 +8,12 @@ const app = express();
 const routes = require('./routes/index')
 
 app.use(cookieParser())
+
+app.use(express.static('public'))
+
+app.set('view-engine', 'ejs')
+
+
 app.use('/', routes)
 
 mongoose.connect(process.env.dbURL, {
