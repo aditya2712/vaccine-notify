d705583c35d6c7cf768ac5b12b07e2f46c5e9201
Aditya Agarwal <aditya.agarwal.20012712@gmail.com>
2021-05-15 22:31:50 +0530

    removed comments

--- src/routes/user.js
+++ src/routes/user.js
@@ -3,9 +3,6 @@ const router = require('express').Router();
 const userController = require('../controllers/user')
 const auth = require('../middlewares/auth')
 
-// console.log(userController1)
-// console.log(userController2)
-
 router.get('/logout', userController.user_logout)
 router.get('/dashboard', auth, userController.user_dashboard_get)
 
